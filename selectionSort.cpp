276cd614515f882a8c85d4b0e97928e855638c04
jraz4444 <105386070+jraz4444@users.noreply.github.com>
2024-02-20 11:49:25 -0500

    Update selectionSort.cpp
    Changed based on discord

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,71 +1,59 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <algorithm> // for swap function
+#include <algorithm>
 
 using namespace std;
 
 // Function to generate a sorted array followed by a reverse sorted array
-void generateReverseSortedArray(int n, int arr[]) {
-    for (int i = n/2, j = n - 1; i < n; i++,j--) {
-        arr[i] = j;
-    }    // Fill the second half of the array with reverse sorted values
-for (int i = n/2 - 1; i >= 0; i--) {
-    arr[i] = n/2 - 1 - i;
-   // Fill array in ascending order for reverse sorting
-}
-
-    
-    // Fill the first half of the array with sorted values
-       // cout << "1 " << arr[i]/2 << endl;
-        // Fill array in ascending order
-
-    // Fill the second half of the array with reverse sorted values
-    for (int i = n / 2, j = n - 1; i < n; i++, j--) {
+void generateReverseSortedArray(long long n, long long arr[]) {
+    for (long long i = n / 2, j = n - 1; i < n; i++, j--) {
         arr[i] = j; // Fill array in descending order
     }
-    
+    for (long long i = n / 2 - 1; i >= 0; i--) {
+        arr[i] = n / 2 - 1 - i; // Fill array in ascending order for reverse sorting
+    }
 }
 
 // Function to generate an almost sorted array
-void generateAlmostSortedArray(int n, int arr[]) {
+void generateAlmostSortedArray(long long n, long long arr[]) {
     srand(time(nullptr));
-    for (int i = 0; i < n; i++) {
+    for (long long i = 0; i < n; i++) {
         arr[i] = i + 1; // Fill array with consecutive numbers
     }
 
     // Randomly swap adjacent elements to create an almost sorted array
-    for (int i = 0; i < n / 10; i++) { // Swap approximately 10% of the elements
-        int idx1 = rand() % n;
-        int idx2 = rand() % n;
+    for (long long i = 0; i < n / 10; i++) { // Swap approximately 10% of the elements
+        long long idx1 = rand() % n;
+        long long idx2 = rand() % n;
         swap(arr[idx1], arr[idx2]);
     }
 }
 
 // Function to generate an array with many duplicates
-void generateDuplicatesArray(int n, int arr[]) {
+void generateDuplicatesArray(long long n, long long arr[]) {
     srand(time(nullptr));
-    for (int i = 0; i < n; i++) {
+    for (long long i = 0; i < n; i++) {
         arr[i] = rand() % 10; // Fill array with random numbers from 0 to 9
     }
 }
 
 // Function to generate a randomly distributed array
-void generateRandomArray(int n, int arr[]) {
+void generateRandomArray(long long n, long long arr[]) {
     srand(time(nullptr));
-    for (int i = 0; i < n; i++) {
+    for (long long i = 0; i < n; i++) {
         arr[i] = rand() % 1000; // Fill array with random numbers from 0 to 999
     }
 }
 
 // Selection sort function
-void selectionsort(int n, int S[], int& comparisons) {
-    int i, j, smallest, temp;
+void selectionsort(long long n, long long S[], long long& comparisons, bool ascending = true) {
+    long long i, j, smallest, temp;
     for (i = 0; i < n - 1; i++) {
         smallest = i;
         for (j = i + 1; j < n; j++) {
             comparisons++; // Increment comparison counter
-            if (S[j] < S[smallest]) {
+            if ((ascending && S[j] < S[smallest]) || (!ascending && S[j] > S[smallest])) {
                 smallest = j;
             }
         }
@@ -76,66 +64,94 @@ void selectionsort(int n, int S[], int& comparisons) {
 
         // Print array after each pass
         cout << "Array after pass " << i + 1 << ": ";
-        for (int k = 0; k < n; k++) {
+        for (long long k = 0; k < n; k++) {
             cout << S[k] << " ";
         }
         cout << endl;
     }
-    cout << "-----------------------" << endl; // Line separating passes
 }
 
 // Function to print an array
-void printArray(int arr[], int n) {
-// Print the generated reverse sorted array
-//cout << "Reverse Sorted Array:" << endl;
-
-// Print the second half of the array
-for (int i = 0; i < n / 2; i++) {
-    cout << arr[i + n / 2] << " ";
-}
-
-// Print the first half of the array
-for (int i = 0; i < n / 2; i++) {
-    cout << arr[i] << " ";
-}
-
-cout << endl;
-
-}
-
-
-int main() {
-    const int n = 10; // Size of the array
-    int sortedArray[n]; 
-    int comparisons;
-    cout << "Sorted Array: " << endl; 
-
-    for (int i = 0; i < n; i++) 
-    {
-       // cout << " I " << i << endl;
-        sortedArray[i] = i;
-        cout << i << " "; 
+void printArray(long long arr[], long long n) {
+    // Print the generated reverse sorted array
+    // Print the second half of the array
+    for (long long i = 0; i < n / 2; i++) {
+        cout << arr[i + n / 2] << " ";
     }
 
-    //printArray(sortedArray,n);
-    int reverseSortedArray[n]; 
-    generateReverseSortedArray(n,reverseSortedArray);
-    
+    // Print the first half of the array
+    for (long long i = 0; i < n / 2; i++) {
+        cout << arr[i] << " ";
+    }
+
     cout << endl;
-    cout << "Reverse Sorted: " << endl; 
-    printArray(reverseSortedArray, n); 
-/*
-    // Test: Sorted then reverse sorted array
-    int sortedThenReverseSortedArr[n];
-    generateSortedThenReverseSortedArray(n, sortedThenReverseSortedArr);
-    cout << "Sorted then Reverse Sorted Array" << endl;
-    comparisons = 0;
-    selectionsort(n, sortedThenReverseSortedArr, comparisons);
+}
+
+// Function to print an array in reverse order
+void printArrayReverse(long long arr[], long long n) {
+    for (long long i = n - 1; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    const long long n = 10; // Size of the array
+    long long sortedArray[n];
+    long long comparisons = 0; // Initialize comparisons counter
+    cout << "Original Sorted Array: ";
+
+    for (long long i = 0; i < n; i++) {
+        sortedArray[i] = i;
+        cout << i << " ";
+    }
+    cout << endl;
+
+    // Test: Unsorted array
+    long long unsortedArray[n];
+    generateRandomArray(n, unsortedArray);
+    cout << "Unsorted Array: ";
+    for (long long i = 0; i < n; i++) {
+        cout << unsortedArray[i] << " ";
+    }
+    cout << endl;
+
+    // Test: Sort unsorted array
+    cout << "Sorting Unsorted Array..." << endl;
+    selectionsort(n, unsortedArray, comparisons);
     cout << "Number of comparisons: " << comparisons << endl;
+
+    // Print sorted array
+    cout << "Sorted Array: ";
+    for (long long i = 0; i < n; i++) {
+        cout << unsortedArray[i] << " ";
+    }
     cout << endl;
-*/
+    cout << endl;
+
+    long long reverseSortedArray[n];
+    generateReverseSortedArray(n, reverseSortedArray);
+
+    cout << "Reverse Sorted Array: ";
+    printArray(reverseSortedArray, n);
+    cout << endl;
+
+    // Test: Sort reverse sorted array in descending order
+    cout << "Sorting Reverse Sorted Array in Descending Order..." << endl;
+    comparisons = 0;
+    selectionsort(n, reverseSortedArray, comparisons, false);
+    cout << "Number of comparisons: " << comparisons << endl;
+
+    // Print sorted array
+    cout << "Sorted Array (Descending Order): ";
+    for (long long i = 0; i < n; i++) {
+        cout << reverseSortedArray[i] << " ";
+    }
+    cout << endl;
+    cout << endl;
+
     // Test: Almost sorted array
-    int almostSortedArr[n];
+    long long almostSortedArr[n];
     generateAlmostSortedArray(n, almostSortedArr);
     cout << "Almost Sorted Array" << endl;
     comparisons = 0;
@@ -144,7 +160,7 @@ int main() {
     cout << endl;
 
     // Test: Array with multiple duplicates
-    int duplicatesArr[n];
+    long long duplicatesArr[n];
     generateDuplicatesArray(n, duplicatesArr);
     cout << "Array with Multiple Duplicates" << endl;
     comparisons = 0;
@@ -153,7 +169,7 @@ int main() {
     cout << endl;
 
     // Test: Randomly distributed array
-    int randomArr[n];
+    long long randomArr[n];
     generateRandomArray(n, randomArr);
     cout << "Randomly Distributed Array" << endl;
     comparisons = 0;
@@ -163,6 +179,4 @@ int main() {
 
     return 0;
 }
-
-
 
